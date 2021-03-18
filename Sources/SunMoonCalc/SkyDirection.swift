80f7e185c6d213d60651d3c851b4826c0a30e679
Werner Freytag <freytag@gmx.de>
2021-03-18 15:08:01 +0100

    add SkyDirection initializer for degrees

--- Sources/SunMoonCalc/SkyDirection.swift
+++ Sources/SunMoonCalc/SkyDirection.swift
@@ -3,8 +3,12 @@ import SwiftToolbox
 
 public enum SkyDirection: CaseIterable {
     case N, NNE, NE, ENE, E, ESE, SE, SSE, S, SSW, SW, WSW, W, WNW, NW, NNW
-    public init(angle: Measurement<UnitAngle>) {
-        let index = Int(floor(normalizeDegrees(angle.converted(to: .degrees).value + 360 / 32) / 360 * 16)) % 16
+    public init(degrees: Double) {
+        let index = Int(floor(normalizeDegrees(degrees + 360 / 32) / 360 * 16)) % 16
         self = SkyDirection.allCases[index]
+    }
+
+    public init(angle: Measurement<UnitAngle>) {
+        self.init(degrees: angle.converted(to: .degrees).value)
     }
 }
